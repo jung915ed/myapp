69466498af481283b99ca506ef5cc46890373166
jung915 <jung915@tc.edu.tw>
2020-03-28 23:01:15 +0800

    java & kotlin practices 79% : kotlin data class and extensions

--- src/main/java/com/jung/train/Ticket.java
+++ src/main/java/com/jung/train/Ticket.java
@@ -6,20 +6,46 @@ public class Ticket {
     int OneWayTickets;
     int RoundTripTickets;
     double discount = RoundTrip * 0.9;
+    int origin;
+    int destination;
+    int price;
 
     public Ticket(int OneWayTickets, int RoundTripTickets) {
         this.OneWayTickets = OneWayTickets;
         this.RoundTripTickets = RoundTripTickets;
     }
 
+    public int getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(int origin) {
+        this.origin = origin;
+    }
+
+    public int getDestination() {
+        return destination;
+    }
+
+    public void setDestination(int destination) {
+        this.destination = destination;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 
     public void Sum() {
-        System.out.printf("you need to paid : "+(OneWay * OneWayTickets + discount * RoundTripTickets));
+        System.out.printf("you need to paid : " + (OneWay * OneWayTickets + discount * RoundTripTickets));
     }
 
     public void TotalSum() {
         System.out.println(
-                        "you have " + OneWayTickets + " ticket(s) for your OneWay trip.\n" +
+                "you have " + OneWayTickets + " ticket(s) for your OneWay trip.\n" +
                         "you have " + RoundTripTickets + " ticket(s) for your Round trip.\n" +
                         "money paid for OneWay trip:  " + OneWay * OneWayTickets + " dollars.\n" +
                         "money paid for Round trip: " + discount * RoundTripTickets + " dollars.\n" +
--- src/main/kotlin/com/jung/train/TestTicket.kt
+++ src/main/kotlin/com/jung/train/TestTicket.kt
@@ -29,3 +29,20 @@ class Tickets(var onewaytickets: Int, var roundtriptickets: Int) {
     }
 }
 
+data class TestTicket(val origin: Int, val destination: Int, val price: Int)
+
+fun main() {
+    var ticket = TestTicket(20, 51, 420)
+    val s = "abcde"
+    println(s.validate())
+    println((1..10).random())
+}
+
+fun String.validate(): Boolean {
+    return this.length >= 6
+}
+
+fun IntRange.random(): Int {
+    val r = Random().nextInt(endInclusive - start) + start
+    return r
+}
